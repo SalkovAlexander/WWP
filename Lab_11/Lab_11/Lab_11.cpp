00ab2c88bddd08c3f2dab802bf4f0ef92ee1c5fa
Alexaner Salkov <a.a.salkov@stud.mospolytech.ru>
2021-10-08 01:40:21 +0300

    Added lab 9, 10, 11

--- Lab_11/Lab_11/Lab_11.cpp
+++ Lab_11/Lab_11/Lab_11.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Windows.h>
 
 int one()
 {
@@ -59,9 +60,129 @@ int two()
     return 0;
 }
 
+int three()
+{
+    double A, B, C;
+    A = 12;
+    B = 9;
+    C = 7;
+
+    if (abs(A - B) < abs(A - C))
+    {
+        std::cout << "B " << abs(A - B) << "\n\n";
+    }
+    else
+    {
+        std::cout << "C " << abs(A - C) << "\n\n";
+    }
+
+    return 0;
+}
+
+int four()
+{
+    double X, Y;
+    X = -10;
+    Y = -2;
+
+    if (X > 0)
+    {
+        if (Y > 0)
+        {
+            std::cout << "I" << "\n\n";
+        }
+        else
+        {
+            std::cout << "IV" << "\n\n";
+        }
+    }
+    else
+    {
+        if (Y > 0)
+        {
+            std::cout << "II" << "\n\n";
+        }
+        else
+        {
+            std::cout << "III" << "\n\n";
+        }
+    }
+
+    return 0;
+}
+
+int five()
+{
+    int A = -44;
+
+    std::cout <<"A - ";
+
+    if (A == 0)
+    {
+        std::cout << "нулевое число" << "\n\n";
+    }
+    else
+    {
+        if (A > 0)
+        {
+            std::cout << "положительное ";
+        }
+        else
+        {
+            std::cout << "отрицательное ";
+        }
+
+        if (A % 2 == 0)
+        {
+            std::cout << "четное число" << "\n\n";
+        }
+        else
+        {
+            std::cout << "нечетное число" << "\n\n";
+        }
+    }
+
+    return 0;
+}
+
+int six()
+{
+    int A = 999;
+
+    std::cout << "A - ";
+
+    if (A % 2 == 0)
+    {
+        std::cout << "четное ";
+    }
+    else
+    {
+        std::cout << "нечетное ";
+    }
+
+    if (A / 10 == 0)
+    {
+        std::cout << "однозначное число" << "\n\n";
+    }
+    else
+    {
+        if (A / 10 < 10)
+        {
+            std::cout << "двузначное число" << "\n\n";
+        }
+        else
+        {
+            std::cout << "трехзначное число" << "\n\n";
+        }
+    }
+
+    return 0;
+}
+
 int main()
 {
-    two();
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
     return 0;
 }
---
+++ Lab_12/Lab_12/Lab_12.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int one()
+{
+    int D, M;
+    D = 24;
+    M = 9;
+
+
+
+    return 0;
+}
+
+int two()
+{
+
+
+    return 0;
+}
+
+int main()
+{
+
+
+    return 0;
+}
